0e703e98dd94fe7e3bbd3a2ebe443721db4b0fa6
Dan <dan.haske@yahoo.com>
2021-01-05 14:58:26 -0600

    fixed Interface-online-course bug and attempted to-Do-List empty alert

--- to-Do-List/script.js
+++ to-Do-List/script.js
@@ -2,41 +2,59 @@ let taskInput = document.getElementById("new-task"); //Add a new task.
 
 $(function () {
     function addTask() {
-    const selectedListName = $("#task").val();
-    console.log("Task is:", selectedListName)
 
-    const targetListId = selectedListName + "List"
-    console.log(targetListId)
+	$("#addButton").click(function () {
+		//check text box for text, if empty alert user to enter task
+		if (!$("#addButton").val()) {
+			const addButtonValue = $("#addButton").val()
+			alert("Please enter a Task!")
+		} else {
+			//check what task list user selected
+			const selectedListName = $("#task").val();
+			console.log("Task is:", selectedListName)
+		
+			//use task to create task list name ID
+			const targetListId = selectedListName + "List"
+			console.log(targetListId)
 
-    const targetList = $(`#${selectedListName}List`)
-    console.log(targetList)
-
-    const inputValue = $("#newTask").val()
-    console.log(inputValue)
-
-    const listItem = $("<li></li>").text(inputValue)
-    const checkBox = $(`<input type="checkbox"></input>`)
-    listItem.append(checkBox) 
-      
-    console.log(listItem)
-
-    targetList.append(listItem)
-  }
-
-  $('#addButton').click(addTask)
-
-  // // let addTask = function () {
-  //   console.log("Add Task...", taskInput);
-  //   //Create a new list item with the text from the #new-task:
-  //   var listItem = createNewTaskElement(taskInput.value);
-
-  //   //Append listItem to incompleteTaskHolder
-  //   incompleteTaskHolder.appendChild(listItem);
-  //   bindTaskEvents(listItem, taskCompleted);
-
-  //   taskInput.value = "";
-  // };
+			//use task to create task list name
+			const targetList = $(`#${selectedListName}List`)
+			console.log(targetList)
+		
+			//grab text from text box
+			const inputValue = $("#newTask").val()
+			console.log(inputValue)
+			
+			//create html structure for lists
+			const listItem = $("<li></li>").text(inputValue)
+			const checkBox = $(`<input type="checkbox"></input>`)
+			listItem.append(checkBox) 
+			  
+			console.log(listItem)
+		
+			targetList.append(listItem)
+		  }
+		
+		  $('#addButton').click(addTask)
+		)
+		}
+	})
+	
 });
+
+
+// To Do List assignment:
+// Done: You must have two separate to-do lists: one for home and one for work.
+// Done: You must have an input box to enter a task.
+// Done: You must have a way of selecting whether the task goes into the home list or the work list.
+// Done: You must have a way of actually adding the task to the chosen list (button, enter key, etc)
+// In progress: If a person attempts to submit (click the button etc) without entering a task, you must display a
+// message saying they must enter a task to add.
+// In progress: If the person tries to do that multiple times, the message should still display only once, not
+// repeatedly.
+// Once a person actually enters a task again, the message should disappear.
+// There must be a way to delete a task that has been added.
+// There must be a way to clear all tasks that have been added.
 
 // clicking Add button adds 
 // task to div class specific of lists, Home and Work
