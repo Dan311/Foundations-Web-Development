d69043e4651e4087c5477370cf21fea93370a53c
Dan <dan.haske@yahoo.com>
2021-02-20 21:04:18 -0600

    second to last updates on Final Project Badlands Outfitters

---
+++ Badlands Final Project/js/script.js
@@ -0,0 +1,11 @@
+$(function() {
+    $("img").hover(function() {
+        $(this).attr("style", "width:150px; height:150px")
+    },function() {
+        $(this).attr("style", "width:125px; height:125px")
+    });
+});
+
+
+
+
